fe1c97b7044cbd81d854256578ee2b1b3d6169ef
Jannik <jw19980309@gmail.com>
2021-02-05 00:51:46 +0100

    Added LocalClient

---
+++ src/config/client.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  ApolloClient,
+  InMemoryCache,
+} from "@apollo/client";
+import { buildAxiosFetch } from "@lifeomic/axios-fetch";
+import axios from "axios";
+import { createHttpLink } from "apollo-link-http";
+
+const localLink = createHttpLink({
+  uri: `${
+    process.env.HOST ||
+    "https://localhost:3000"
+  }/api/graphql`,
+  fetch: buildAxiosFetch(
+    axios as any,
+  ) as any,
+});
+
+export const localClient = new ApolloClient(
+  {
+    link: localLink as any,
+    cache: new InMemoryCache(),
+  },
+);
