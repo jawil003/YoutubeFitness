ea49e640ebdf8b5e394982f54e81bdabd1e972df
Jannik <jw19980309@gmail.com>
2021-02-05 12:45:56 +0100

    Updated YoutubeVideo Type

--- src/graphql/youtubeVideo.type.ts
+++ src/graphql/youtubeVideo.type.ts
@@ -5,32 +5,72 @@ import {
 
 @ObjectType()
 class YoutubeThumbnailsElement {
-  @Field()
+  @Field({
+    description:
+      "The Url from which the Image can be fetched",
+    nullable: false,
+  })
   url!: string;
-  @Field()
+  @Field({
+    description:
+      "The Height of the Image",
+    nullable: false,
+  })
   width!: number;
-  @Field()
+  @Field({
+    description:
+      "The Width of the Image",
+    nullable: false,
+  })
   height!: number;
 }
 
 @ObjectType()
 class YoutubeThumbnails {
-  @Field()
+  @Field({
+    description:
+      "The Thumbnail in the Default Resolution",
+    nullable: false,
+  })
   default!: YoutubeThumbnailsElement;
-  @Field()
+  @Field({
+    description:
+      "The Thumbnail in Medium Resolution",
+    nullable: false,
+  })
   medium!: YoutubeThumbnailsElement;
-  @Field()
+  @Field({
+    description:
+      "The Thumbnail in High Resolution",
+    nullable: false,
+  })
   high!: YoutubeThumbnailsElement;
-  @Field()
+  @Field({
+    description:
+      "The Thumbnail in the Standard Resolution",
+    nullable: false,
+  })
   standard!: YoutubeThumbnailsElement;
 }
 
 @ObjectType()
 export default class YoutubeVideo {
-  @Field()
+  @Field({
+    description:
+      "The Video Id of the Youtube Video",
+    nullable: false,
+  })
   id!: string;
-  @Field()
+  @Field({
+    description:
+      "The Title of the Youtube Video in the Original Language",
+    nullable: false,
+  })
   title!: string;
-  @Field(() => YoutubeThumbnails)
+  @Field(() => YoutubeThumbnails, {
+    description:
+      "The Thumbnails for the Video in different Sizes",
+    nullable: false,
+  })
   thumbnails!: YoutubeThumbnails;
 }
