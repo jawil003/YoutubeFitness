0b26860720125a67c5deb509a55585601b5e291d
Jannik <jw19980309@gmail.com>
2021-02-07 22:28:45 +0100

    Updated tests with MockServer for Frontend Tests

---
+++ src/__tests__/indexPage.test.ts
@@ -0,0 +1,28 @@
+import { localApiClient } from "../config/client";
+import startServer from "../mocks/graphql/apolloMockServer";
+
+const query = `query getYoutubeVideoMetadata($youtubeVideoId: String!) {
+  videoData: youtubeVideoMeadata(youtubeVideoId: $youtubeVideoId) {
+    id
+    title
+    thumbnails {
+      default {
+        url
+      }
+    }
+  }
+}
+`;
+
+beforeAll(
+  async () => await startServer(),
+);
+
+test("Test Mockup Server Graphql Api", async () => {
+  await localApiClient.post(
+    "/graphql",
+    {
+      query,
+    },
+  );
+});
